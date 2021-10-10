8e1816b1f328c03ea3a34f58ae38d30060d4c3e3
ShahriyarHussain <shossainshihab36@gmail.com>
2021-10-11 00:57:45 +0600

    Method Referencing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,68 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Dennis", age: "25" },
+      { name: "Ritchie", age: "25" },
+      { name: "Linus", age: "25" },
+    ],
+  };
 
-    state = {
-        persons: [
-            { name: 'Dennis', age:"25"},
-            { name: 'Ritchie', age:"25"},
-            { name: 'Linus', age:"25"}
-        ]
-    };
+  randomNumGen = () => {
+    return Math.floor(Math.random() * 30);
+  };
 
-    randomNumGen = () => {
-        return Math.floor(Math.random() * 30);
-    };
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: this.randomNumGen() },
+        { name: "Zax", age: this.randomNumGen() },
+        { name: "Baldur", age: this.randomNumGen() },
+      ]
+    });
+  };
 
-    switchNameHandler = (newName) => {
-        this.setState(
-            { persons: [
-                {name: newName, age: this.randomNumGen()},
-                {name: 'Zax', age: this.randomNumGen()},
-                {name: 'Baldur', age: this.randomNumGen()},
-            ]
-        });
-    };
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Ella", age: 45 },
+        { name: event.target.value, age: 35 },
+        { name: "Baldur", age: 22 },
+      ]
+    });
+  }
 
-    render() {
-        return (
-            <div className="App">
-            <h1>Hello, I'm React</h1>
-            <h1>But Who are you ?</h1>
-            <button onClick={() => this.switchNameHandler("Ziga")}> Or Are You ? </button>
-            <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}/>
-            <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            click={this.switchNameHandler.bind(this, 'beatrice')}>
-                I am different because I have a hobby
-            </Person>
-            <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}/>
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, I'm React</h1>
+        <h1>But Who are you ?</h1>
+        <button onClick={() => this.switchNameHandler("Ziga")}>
+          Or Are You ?
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, "beatrice")}
+        >
+          I am different because I have a hobby
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "tyla")}
+          changed={this.nameChangedHandler}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const person = props => {
-    return (
-        <div>
-        <p onClick={props.click}>
+const person = (props) => {
+  return (
+    <div>
+      <p onClick={props.click}>
         I'm <strong>{props.name}</strong> ! I am <em>{props.age}</em>
-        </p>
-        <p>{props.children}</p>
-        </div>
-
-    );
-}
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed}></input>
+    </div>
+  );
+};
 
 export default person;
